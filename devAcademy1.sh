4dde49e1a134547d7cc9ea3daea3ef243b5b5aed
Jeison <jeisoncastro2@gmail.com>
2016-07-25 10:45:01 -0500

    update devAcademy1.sh

---
+++ devAcademy1.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+zenity --info --title="devAcademy" --text="Bienvenido¡ Es importante actualizar su sistema"
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install figlet -y
+sudo apt-get install fortune cowsay
+figlet devAcademy -c
+if (`zenity --question --text="continuar instalación"`)
+then
+(
+echo "10" ; sleep 1
+echo "# instalando apache..." ; sleep 1
+sudo apt-get install apache2 -y
+zenity --notification\
+    --window-icon="info" \
+    --text="se ha instalado apache"
+echo "25" ; sleep 1
+echo "# instalando git" ; sleep 1
+sudo apt-get install git -y 
+zenity --notification\
+    --window-icon="info" \
+    --text="se ha instalado git..."
+echo "35" ; sleep 1
+echo "# instalando tmux..." ; sleep 1
+sudo apt-get install tmux -y
+echo "40" ; sleep 1
+echo "# instalando ssh..." ; sleep 1
+sudo apt-get install openssh-server -y
+echo "50" ; sleep 1
+echo "# instalando sshfs..." ; sleep 1
+sudo apt-get install sshfs
+echo "66" ; sleep 1
+echo "# instalando nodejs..." ; sleep 1
+sudo apt-get install nodejs -y
+echo "75" ; sleep 1
+echo "# instalando nmap..." ; sleep 1
+sudo apt-get install nmap -y
+zenity --notification\
+    --window-icon="info" \
+    --text="se ha instalado ssh y nodejs"
+echo "80" ; sleep 1
+echo "# instalando curl..." ; sleep 1
+sudo apt-get install curl
+echo "85" ; sleep 1
+echo "# instalando zsh [pulsar intro]" ; sleep 1
+sudo apt-get install zsh -y
+curl -L http://install.ohmyz.sh | sh
+sudo apt-get update 
+sudo add-apt-repository ppa:webupd8team/sublime-text-3 
+echo "89" ; sleep 1
+echo "# instalando sublime text..." ; sleep 1
+sudo apt-get install sublime-text-installer
+zenity --notification\
+    --window-icon="info" \
+    --text="se ha instalado sublime text y zsh"
+echo "90" ; sleep 1
+echo "# instalando  heroku..." ; sleep 1
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh 
+zenity --notification\
+    --window-icon="info" \
+    --text="se ha instalado heroku"
+echo "95" ; sleep 1
+echo "# instalando  rbenv..." ; sleep 1
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-b$
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/$
+sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
+zenity --notification\
+    --window-icon="info" \
+    --text="se ha instalado rbenv"
+echo "100" ; sleep 1
+echo "# completado devAcademy1.sh" ; sleep 1
+zenity --info --title="devAcademy" --text="La instalación a finalizado correctamente"
+zenity --info --title="devAcademy" --text="instala el siguiente Script"
+exec $SHELL
+) |
+zenity --progress \
+  title="Actualizando los registros del sistema" \
+  text="Rastreando los registros de los correos..." \
+  percentage=0
+if [ "$?" = -1 ] ; then
+zenity --error \
+text="Actualización cancelada."
+fi
+else
+figlet devAcademy -c
+fortune | cowsay -f tux -t ¿Que paso ?
+fi
+
+
+
+
---
+++ devAcademy2.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+sudo apt-get update 
+if (`zenity --question --text="continuar instalación"`)
+then
+(
+echo "10" ; sleep 1
+echo "# instalando ruby ..." ; sleep 1
+rbenv install 2.3.1
+rbenv global 2.3.1
+zenity --notification\
+    --window-icon="info" \
+    --text="se ha instalado ruby "
+gem install rails
+#sudo gem install pg
+sudo gem update --system
+sudo gem update
+zenity --notification\
+    --window-icon="info" \
+    --text="se ha instalado ruby on rails"
+echo "20" ; sleep 1
+echo "# instalando screenfetch..." ; sleep 1
+sudo apt-get install screenfetch -y
+echo "30" ; sleep 1
+echo "# instalando vim..." ; sleep 1
+sudo apt-get install vim -y
+zenity --notification\
+    --window-icon="info" \
+    --text="se ha instalado vim"
+sudo apt-get update 
+echo "45" ; sleep 1
+echo "# instalando postgresql..." ; sleep 1
+sudo apt-get install postgresql postgresql-client postgresql-contrib libpq-dev -y
+#zenity --info --title="devAcademy" --text="Ingresa tu usuario a PostgreSQL"
+sudo -u postgres createuser --interactive
+zenity --notification\
+    --window-icon="info" \
+    --text="se ha instalado PostgreSQL"
+echo "50" ; sleep 1
+echo "# instalando libreria npm..." ; sleep 1
+sudo apt-get install npm -y
+#sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql -y
+#sudo apt-get install php-cli -y
+#sudo apt-get install composer -y
+zenity --notification\
+    --window-icon="info" \
+    --text="se ha instalado npm..."
+echo "60" ; sleep 1
+echo "# instalando mysql..." ; sleep 1
+sudo dpkg --configure -a
+sudo apt-get install mysql-server -y
+sudo dpkg --configure -a
+zenity --notification\
+    --window-icon="info" \
+    --text="se ha instalado mysql"
+sudo apt-get update
+echo "100" ; sleep 1
+echo "# completado" ; sleep 1
+zenity --info --title="devAcademy" --text="La instalación a finalizado correctamente"
+figlet devAcademy -c
+screenfetch
+) |
+zenity --progress \
+  title="Actualizando los registros del sistema" \
+  text="Rastreando los registros de los correos..." \
+  percentage=0
+if [ "$?" = -1 ] ; then
+zenity --error \
+text="Actualización cancelada."
+fi
+else
+figlet devAcademy -c
+fortune | cowsay -f tux -t ¿Que paso ?
+fi
